7e333ab4c2f160d904d9962ca14625d59e0cc8b5
l-tiezerin <lucastiezerin@gmail.com>
2026-01-01 00:48:00 -0300

    loan features

--- src/loan_generator.py
+++ src/loan_generator.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import random
+import math
 from client_generator import client_generator
 
 def loan_generator(clients):
@@ -10,7 +12,18 @@ def loan_generator(clients):
     weights /= weights.sum()
 
     loan_quantity_array = rng.choice(possible_loan_quantity, size=50_000, p=weights)
-    loan_id_array = rng.choice(np.arange(1_000, 10_000_001), size=loan_quantity_array.sum(), replace=False)
+    total_loan_quantity = loan_quantity_array.sum()
+
+    loan_id_array = rng.choice(np.arange(1_000, 10_000_001), size=total_loan_quantity, replace=False)
+    
+    log_min = math.log(1_000)
+    log_max = math.log(10_000_000)
+    random.seed(42)
+
+    loan_amount_array = [
+        round(math.exp(random.uniform(log_min, log_max)), -2)
+        for _ in range(total_loan_quantity)
+    ]
 
     df_loans = clients.copy()
     df_loans['loan_quantity'] = loan_quantity_array
@@ -20,7 +33,8 @@ def loan_generator(clients):
         .drop('loan_quantity', axis=1)
     )
     df_loans['loan_id'] = loan_id_array
+    df_loans['loan_amount'] = loan_amount_array
 
-    return print(df_loans)
+    return print(df_loans, df_loans['loan_amount'].describe())
 
 loan_generator(pd.DataFrame({'id': np.arange(1, 50_001)}))
